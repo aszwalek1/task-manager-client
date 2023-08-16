0479fad8fba291c12d0dfe62b19579b2cc48b7aa
Alicja Szwalek <aszwalek1@sheffield.ac.uk>
2023-08-16 13:58:18 +0100

    delet unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState();
+
+  const getTasks = async () => {
+
+    try {
+      const response = await api.get("/api/v1/tasks");
+      console.log(response.data);
+      setTasks(response.data);
+    }
+    catch(err) {
+      console.log(err);
+    }
+    
+  }
+
+  useEffect(() => {
+    getTasks();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/api/axiosConfig.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: '2U1lSmDjwijnD7sPCWHYTGo5PdV_6ZMNd39izAyrGGbakK6Jt',
+    headers: {"ngrok-skip-browser-warning": "true"}
+})
