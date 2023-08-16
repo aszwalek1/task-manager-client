d7c835b7fe34e153f6b92decfb64ac49aa63df08
Alicja Szwalek <aszwalek1@sheffield.ac.uk>
2023-08-16 14:46:28 +0100

    changed baseURL to connect with backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: '2U1lSmDjwijnD7sPCWHYTGo5PdV_6ZMNd39izAyrGGbakK6Jt',
+    baseURL: 'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
